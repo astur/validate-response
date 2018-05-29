8a4088a696c4e85c68f93bd8c9f5cd464473a1d9
astur <astur@yandex.ru>
2018-05-29 23:02:10 +0300

    response type check

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = code => response => {
+    if(!(response instanceof require('http').IncomingMessage)) throw new Error('IncomingMessage expected');
     if(response.statusCode === code) return;
     throw new Error('ValidateResponceError');
 };
--- test.js
+++ test.js
@@ -2,11 +2,15 @@ const test = require('ava');
 const m = require('.');
 const request = require('scra');
 
-test('validate-response', async t => {
+test('base', async t => {
     await request('httpbin.org/status/200')
         .then(res => t.notThrows(() => m(200)(res)));
     await request('httpbin.org/status/500')
         .then(res => t.throws(() => m(200)(res)));
     await request('httpbin.org/status/404')
         .then(res => t.throws(() => m(400)(res)));
+});
+
+test('response type check', t => {
+    t.throws(() => m(200)({statusCode: 200}));
 });
