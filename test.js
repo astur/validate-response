e17d1bc86d68527cf8f620bc665e7138cd80af87
astur <astur@yandex.ru>
2018-05-29 22:52:33 +0300

    tests with real requests by scra

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 const test = require('ava');
 const m = require('.');
+const request = require('scra');
 
-test('validate-response', t => {
-    t.notThrows(() => m(200)({statusCode: 200}));
-    t.throws(() => m(200)({statusCode: 500}));
-    t.throws(() => m(400)({statusCode: 200}));
+test('validate-response', async t => {
+    await request('httpbin.org/status/200')
+        .then(res => t.notThrows(() => m(200)(res)));
+    await request('httpbin.org/status/500')
+        .then(res => t.throws(() => m(200)(res)));
+    await request('httpbin.org/status/404')
+        .then(res => t.throws(() => m(400)(res)));
 });
