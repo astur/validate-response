7566cd9e50e331b2ec02bdb26504ce81c496dc1c
astur <astur@yandex.ru>
2018-05-30 15:07:07 +0300

    multi valid code support

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-module.exports = code => response => {
-    if(!(response instanceof require('http').IncomingMessage)) throw new TypeError('IncomingMessage expected');
-    if(response.statusCode === code) return;
-    throw new Error(`Expected status code ${code} (${response.statusCode} found)`);
+const arfy = require('arfy');
+module.exports = (...codes) => {
+    codes = arfy(...codes);
+    if(!codes.length) codes = [200];
+    return response => {
+        if(!(response instanceof require('http').IncomingMessage)) throw new TypeError('IncomingMessage expected');
+        if(codes.includes(response.statusCode)) return;
+        throw new Error(`Expected status code in [${codes.join(', ')}] (${response.statusCode} found)`);
+    };
 };
--- test.js
+++ test.js
@@ -2,13 +2,35 @@ const test = require('ava');
 const m = require('.');
 const request = require('scra');
 
-test('base', async t => {
+test('valid status codes', async t => {
+    await request('httpbin.org/status/200')
+        .then(res => t.notThrows(() => m()(res)));
+
     await request('httpbin.org/status/200')
         .then(res => t.notThrows(() => m(200)(res)));
+
+    await request('httpbin.org/status/200')
+        .then(res => t.notThrows(() => m(200, 300)(res)));
+
+    await request('httpbin.org/status/200')
+        .then(res => t.notThrows(() => m([200])(res)));
+
+    await request('httpbin.org/status/200')
+        .then(res => t.notThrows(() => m([200, 400])(res)));
+});
+
+test('invalid status codes', async t => {
     await request('httpbin.org/status/500')
-        .then(res => t.throws(() => m(200)(res), 'Expected status code 200 (500 found)'));
-    await request('httpbin.org/status/404')
-        .then(res => t.throws(() => m(400)(res)));
+        .then(res => t.throws(() => m(200)(res), 'Expected status code in [200] (500 found)'));
+
+    await request('httpbin.org/status/200')
+        .then(res => t.throws(() => m(400)(res), 'Expected status code in [400] (200 found)'));
+
+    await request('httpbin.org/status/500')
+        .then(res => t.throws(() => m(200, 300, 400)(res), 'Expected status code in [200, 300, 400] (500 found)'));
+
+    await request('httpbin.org/status/500')
+        .then(res => t.throws(() => m([200, 300, 400])(res), 'Expected status code in [200, 300, 400] (500 found)'));
 });
 
 test('response type check', t => {
