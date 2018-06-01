dec48660909f602878acf0cd41154e9cbd6f4a71
astur <astur@yandex.ru>
2018-06-01 20:04:51 +0300

    refactoring of error messages

--- index.js
+++ index.js
@@ -16,34 +16,40 @@ module.exports = (...options) => {
                 return n;
             }) :
         null;
-
+    // ValidateResponceError
     return response => {
         if(!(response instanceof require('http').IncomingMessage)) throw new TypeError('IncomingMessage expected');
+
+        const reasons = [];
+
         if(options.codes && !options.codes.includes(response.statusCode)){
-            throw new Error(`Expected status code in [${options.codes.join(', ')}] (${response.statusCode} found)`);
+            reasons.push(`Expected status code in [${options.codes.join(', ')}] (${response.statusCode} found)`);
         }
         if(options.checkJSON && response.headers['content-type'] === 'application/json' && !type.isObject(response.body)){
-            throw new Error(`Expected json-parsed object in body (${type(response.body)} found)`);
+            reasons.push(`Expected json-parsed object in body (${type(response.body)} found)`);
         }
         if(type.isNumber(options.contentLength) && options.contentLength !== +response.headers['content-length']){
-            throw new Error(`Expected content length ${options.contentLength} (${response.headers['content-length']} found)`);
+            reasons.push(`Expected content length ${options.contentLength} (${response.headers['content-length']} found)`);
         }
         if(type.isArray(options.contentLength) && (+response.headers['content-length'] < options.contentLength[0] || +response.headers['content-length'] > options.contentLength[1])){
-            throw new Error(`Expected content length in range ${options.contentLength.join('-')} (${response.headers['content-length']} found)`);
+            reasons.push(`Expected content length in range ${options.contentLength.join('-')} (${response.headers['content-length']} found)`);
         }
         if(type.isRegExp(options.bodyMatch) && type.isString(response.body) && !options.bodyMatch.test(response.body)){
-            throw new Error(`Expected body string match to ${options.bodyMatch}`);
+            reasons.push(`Expected body string match to ${options.bodyMatch}`);
         }
         if(type.isFunction(options.validator)){
-            let isValid;
             try {
-                isValid = options.validator(response);
+                const isValid = options.validator(response);
+                if(isValid){
+                    reasons.push(`Custom validator failed with messahe: "${isValid}"`);
+                }
             } catch(e){
-                throw new Error(`Custom validator threw "${e}"`);
+                reasons.push(`Custom validator threw "${e}"`);
             }
-            if(isValid){
-                throw new Error(`Custom validator failed with messahe: "${isValid}"`);
-            }
+        }
+
+        if(reasons.length){
+            throw new Error(reasons[0]);
         }
     };
 };
